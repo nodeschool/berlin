226b712e1c3ddd8e58dbbe3e37f2e828c7f7fc5c
Finn <derfinn@gmail.com>
2015-04-22 21:25:30 +0200

    ordered stickers for international day

--- budget.js
+++ budget.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-console.log('Budget:',
+console.log('Budget:',(
      0.00 // budget.js - keeping the books for nodeschool berlin
           // inspired by https://github.com/brooklynjs/brooklynjs.github.io/blob/master/budget.js
 
@@ -7,5 +7,5 @@ console.log('Budget:',
 +  230.00 // sponsoring DigitalOcean
 -  133.28 // Costs for the WikiMedia venue
 -   39.47 // NodeSchool stickers
-
-,'Euros')
+-   98.54 // NodeSchool International Day stickers
+),'Euros')
