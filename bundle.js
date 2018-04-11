ab661b8acbbcc365c020e5209b388193b04cb60f
Travis CI User <travis@example.org>
2018-04-11 19:52:30 +0000

    Updates

--- bundle.js
+++ bundle.js
@@ -210,8 +210,8 @@ function view(e,t){return e.title!==TITLE&&t(e.events.DOMTITLECHANGE,TITLE),func
 
 },{"13":13,"18":18,"19":19,"20":20,"67":67}],70:[function(require,module,exports){
 module.exports={
-  "startDate": "2018-04-26",
-  "eventLink": "https://www.meetup.com/opentechschool-berlin/events/249501127/",
+  "startDate": "2018-04-25",
+  "eventLink": "https://www.meetup.com/opentechschool-berlin/events/249671843/",
   "mentors": [
     "finnp",
     "lrlna",
