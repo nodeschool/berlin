419d7c35544019acbaa3e54c5e63746a47883dc3
liv <olivia@fastmail.com>
2018-09-25 09:56:13 +0200

    unsummerbreakify

--- views/main.js
+++ views/main.js
@@ -1,7 +1,7 @@
 var html = require('choo/html')
-// var differenceInCalendarDays = require('date-fns/difference_in_calendar_days')
-// var addWeeks = require('date-fns/add_weeks')
-// var dateFormat = require('date-fns/format')
+var differenceInCalendarDays = require('date-fns/difference_in_calendar_days')
+var addWeeks = require('date-fns/add_weeks')
+var dateFormat = require('date-fns/format')
 
 var TITLE = 'nodeschool berlin'
 
@@ -16,7 +16,7 @@ function view (state, emit) {
         <img class="w5" src="assets/logo.png" alt="logo" />
         <h1 class="b f1 f-headline-ns db mt0 mb0">nodeschool</h1>
         <h2 class="f2 f1-ns mt0">berlin</h2>
-        ${summerBreak()}
+        ${nextEvent()}
       </div>
       <div class="cf mw8 center pb5">
         <div class="fl ph3 w-100 w-33-ns tc">
@@ -43,19 +43,19 @@ function view (state, emit) {
     </body>
   `
 
-  // function nextEvent () {
-  //   var startDate = new Date(state.startDate)
-  //   var today = new Date()
-  //   var days = differenceInCalendarDays(today, startDate)
-  //   var weeks = Math.max(0, Math.ceil(days / 7))
-  //   if (weeks % 2 !== 0) weeks++
-  //   var nextDate = addWeeks(startDate, weeks)
-  //   var formattedDate = dateFormat(nextDate, 'Do MMMM YYYY')
-  //   return html`<div>
-  //     <div class="f3">We meet Wednesdays every 2 weeks @ Mozilla Berlin</div>
-  //     <a href="${state.eventLink}" class="f3 link dim ba bw1 ph3 pv2 mv3 mb2 dib black">RSVP here for ${formattedDate}</a>
-  //   </div>`
-  // }
+  function nextEvent () {
+    var startDate = new Date(state.startDate)
+    var today = new Date()
+    var days = differenceInCalendarDays(today, startDate)
+    var weeks = Math.max(0, Math.ceil(days / 7))
+    if (weeks % 2 !== 0) weeks++
+    var nextDate = addWeeks(startDate, weeks)
+    var formattedDate = dateFormat(nextDate, 'Do MMMM YYYY')
+    return html`<div>
+      <div class="f3">We meet Wednesdays every 2 weeks @ Mozilla Berlin</div>
+      <a href="${state.eventLink}" class="f3 link dim ba bw1 ph3 pv2 mv3 mb2 dib black">RSVP here for ${formattedDate}</a>
+    </div>`
+  }
 
   function summerBreak () {
     return html`<div>
